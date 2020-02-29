5ef770b93d46148c156b02c27b6dafa38a55affc
Sergei Zotov <zsv.main@gmail.com>
2020-02-29 23:03:21 +0500

    feat dream-team realization

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,43 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = '';  
+  if (!Array.isArray(members)) {    
+    return false
+  }
+  for (const item of members) {     
+    if (typeof(item) == 'string') {
+        let el = item.trim();      
+        result += el[0].toUpperCase();      
+    }    
+  }
+  return result.split('').sort().join('');
 };
+
+// ADGJKMNPRSTW
+// ADGJKMNPRSTW
+// const members = [
+//   '   William Alston ',
+//   ' Paul Benacerraf',
+//   '  Ross Cameron',
+//   '       Gilles Deleuze',
+//   '  Arda Denkel ',
+//   '  Michael Devitt',
+//   '  Kit Fine',
+//   ' Nelson Goodman',
+//   'David Kolb',
+//   '   Saul Kripke',
+//   '  Trenton Merricks',
+//   '  Jay Rosenberg',
+// ]
+
+// function createDreamTeam(members) {
+//   let result = '';
+//   for (const item of members) {
+//     let el = item.trim();    
+//     if (el && /[a-zA-Z]/.test(el)) {
+//       result+= el[0].toUpperCase();
+//     }
+//   }
+//   console.log(result.split('').sort().join(''));
+// };
+
+// createDreamTeam(members)
