ba1c182ac045ab7e58e15994632bcde28145b5a8
Sergei Zotov <zsv.main@gmail.com>
2020-03-07 22:38:29 +0500

    feat: add realize recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,74 +1,54 @@
-// module.exports = class DepthCalculator {
-//     calculateDepth(arr) {
-//         let result = 0;
-//         let a = [];
-    
-//         function deep(array) {        
-//             if (Array.isArray(array)) {
-//                 array.forEach((element, i) => {
-//                     if (Array.isArray(element)) {
-//                         a.push('[')                    
-//                         return deep(element);
-//                     }                
-//                 });
-//                 a.push(']')               
-//             }        
+module.exports = class DepthCalculator {
+    calculateDepth(arr) {   
+        let result = 0;
+        arr.forEach( (item, i ) => {
+            if(Array.isArray(item)) {
+                let timeTmp = this.calculateDepth(item);
+                result  = result > timeTmp ? result : timeTmp;
+            }
+        })
+        return result+1;                
+    }
+};
+
+// const arr = [1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]
+
+// function calculateDepth(arr) {
+//     let result = 1;
+//     let a = [];
+
+//     // function deep(array) {        
+//         if (Array.isArray(arr)) {
+//             // a.push('[')                    
+//             arr.forEach((element, i) => {
+//                 if (Array.isArray(element)) {
+//                     a.push('[')                    
+//                     return calculateDepth(element);
+//                 }                
+//             });
+//             a.push(']')
+//             // if (result <= itog) result =  result + itog;    
+//         }        
+//     // }
+//     // deep(arr)
+//     let tmp = 0;
+//     let prev = 0;
+//     let text = '';
+//     a.forEach(element => {
+//         text += element + '';
+//         console.log(text)
+//         console.log('--------------')
+//         if (element == '[') {
+//             tmp++;
+//         } else {            
+//             if (result < tmp ) result = result + tmp;
+//             tmp = 0;
 //         }
-//         deep(arr)
-//         let tmp = 0;
-//         let prev = 0;
-//         a.forEach(element => {
-            
-//             if (element == '[') {
-//                 tmp++;
-//             } else {            
-//                 if (result < tmp ) result = tmp;
-//                 tmp = 0;
-//             }
-//         });  
-        
-//         return result+1;
-//     }
-// };
+//     });  
+//     console.log(text);
+//     return calculateDepth(a);
 
-const arr = [1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]
+// }
 
-function calculateDepth(arr) {
-    let result = 1;
-    let a = [];
-
-    // function deep(array) {        
-        if (Array.isArray(arr)) {
-            // a.push('[')                    
-            arr.forEach((element, i) => {
-                if (Array.isArray(element)) {
-                    a.push('[')                    
-                    return calculateDepth(element);
-                }                
-            });
-            a.push(']')
-            // if (result <= itog) result =  result + itog;    
-        }        
-    // }
-    // deep(arr)
-    let tmp = 0;
-    let prev = 0;
-    let text = '';
-    a.forEach(element => {
-        text += element + '';
-        console.log(text)
-        console.log('--------------')
-        if (element == '[') {
-            tmp++;
-        } else {            
-            if (result < tmp ) result = result + tmp;
-            tmp = 0;
-        }
-    });  
-    console.log(text);
-    return calculateDepth(a);
-
-}
-
-console.log(calculateDepth(arr));
+// console.log(calculateDepth(arr));
 
