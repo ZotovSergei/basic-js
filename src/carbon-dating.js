e1a95783aedf620fa0d357ba763f5950c9198e8c
Sergei Zotov <zsv.main@gmail.com>
2020-03-09 13:51:39 +0500

    feat: add realize carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,12 +1,12 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity == 'string') {    
-    const sample = sampleActivity*1 || false;  
-    if (sample <= 0 || sample >= 15 || !sample) return false;
-    return (Math.log((15/sample))) /((Math.log(2))/5730);
-  }
-  return false
-  
+    if (typeof sampleActivity == 'string') {
+        const sample = parseFloat(sampleActivity) || false;
+        if (sample <= 0 || sample >= 15 || !sample) return false;
+        return Math.ceil(Math.log(MODERN_ACTIVITY / sample) * HALF_LIFE_PERIOD / 0.693);
+        // Math.log(15 / sample) / (Math.log(2) / 5730)
+    }
+    return false
 };
