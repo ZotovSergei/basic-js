796f425a1772a1bc30f09838f2487b7a0a72dc04
Sergei Zotov <zsv.main@gmail.com>
2020-03-09 20:57:45 +0500

    feat: add realize what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,31 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!date) return 'Unable to determine the time of year!';
+    if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error();
+    // if (date.getYear() <= 1970) return false;
+    let month = 12 / date.getMonth();
+    switch (date.getMonth()) {
+        case 11:
+        case 12:
+        case 1:
+            return 'winter';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            return 'spring'
+            break;
+        case 5:
+        case 6:
+        case 7:
+            return 'summer'
+            break;
+        case 8:
+        case 9:
+        case 10:
+            return 'autumn'
+            break;
+        default:
+            return 'winter';
+            break;
+    }
 };
